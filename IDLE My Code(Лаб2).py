63ff4d6428489825eecdfc42f4e5b96ca0b6674e
MelnykVladislav <71428320+MelnykVladislav@users.noreply.github.com>
2020-09-22 10:06:37 +0300

    Add files via upload

---
+++ IDLE My Code(Лаб2).py
@@ -0,0 +1,20 @@
+name = input("Как Вас зовут? ")
+print("Привет,", name)
+
+
+
+years = input("Сколько тебе лет? ")
+print (years ," , Понятно")
+
+
+
+a = "Данные были внесены в систему"; print (a)
+
+b = "Welcome"; print (b)
+
+
+
+input('Press ENTER to exit') 
+
+
+  
